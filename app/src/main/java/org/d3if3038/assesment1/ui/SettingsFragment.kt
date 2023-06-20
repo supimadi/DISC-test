edcc6fc540474521e5fffd1e92f09676914b442e
Lukman Hakim <hlukman156@gmail.com>
2023-06-20 15:52:24 +0700

    remove unused files

--- app/src/main/java/org/d3if3038/assesment1/ui/SettingsFragment.kt
+++ app/src/main/java/org/d3if3038/assesment1/ui/SettingsFragment.kt
@@ -1,6 +1,8 @@
 package org.d3if3038.assesment1.ui
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.transition.TransitionInflater
 import org.d3if3038.assesment1.R
@@ -23,5 +25,15 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         preferenceManager.preferenceDataStore = settingDataStore
         setPreferencesFromResource(R.xml.prefrences, rootKey)
+
+//        val tokenPreference: EditTextPreference? = findPreference(
+//            getString(R.string.token_admin_preferences)
+//        )
+//
+//        tokenPreference!!.setOnPreferenceClickListener {
+//            Toast.makeText(requireContext(), getString(R.string.feature_finding), Toast.LENGTH_LONG).show()
+//            true
+//        }
+
     }
 }
--- app/src/main/java/org/d3if3038/assesment1/ui/welcome/WelcomeViewModel.kt
+++ app/src/main/java/org/d3if3038/assesment1/ui/welcome/WelcomeViewModel.kt
@@ -6,14 +6,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.work.ExistingWorkPolicy
-import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.workDataOf
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.d3if3038.assesment1.model.personality.Profile
-import org.d3if3038.assesment1.network.ApiStatus
 import org.d3if3038.assesment1.network.FunFactsWorker
 import org.d3if3038.assesment1.network.PersonalityApi
 import java.util.concurrent.TimeUnit
@@ -35,18 +33,22 @@ class WelcomeViewModel : ViewModel() {
     fun fireFactNotif(app: Application) = viewModelScope.launch(Dispatchers.IO) {
         try {
             val funFacts = PersonalityApi.service.getFunFacts()
-            val workerRequest = OneTimeWorkRequestBuilder<FunFactsWorker>()
-                .setInitialDelay(2, TimeUnit.MINUTES)
+            val workerRequest = PeriodicWorkRequestBuilder<FunFactsWorker>(
+                1, TimeUnit.DAYS,
+                10, TimeUnit.HOURS
+                )
                 .setInputData(workDataOf(
                     "FUN_FACTS" to funFacts[Random.nextInt(0, funFacts.size)].facts
                 ))
                 .build()
 
-            WorkManager.getInstance(app).enqueueUniqueWork(
-                FunFactsWorker.WORK_NAME,
-                ExistingWorkPolicy.REPLACE,
-                workerRequest
-            )
+//            WorkManager.getInstance(app).enqueueUniqueWork(
+//                FunFactsWorker.WORK_NAME,
+//                ExistingWorkPolicy.REPLACE,
+//                workerRequest
+//            )
+
+            WorkManager.getInstance(app).enqueue(workerRequest)
 
         } catch (e: Exception) {
             Log.e("NOTIFICATION", "Failure: ${e.message}")
